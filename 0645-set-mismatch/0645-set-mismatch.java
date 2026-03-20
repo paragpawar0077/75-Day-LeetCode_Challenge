528b7009bda952a47a0b46cea32f6b541ef868be
Parag Pawar <paragpawar0077@gmail.com>
2026-03-20 23:10:59 +0530

    Time: 4 ms (45%), Space: 47.5 MB (86.21%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+         List<Integer> answer = new ArrayList<>();
+     cyclicsort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != i+1){
+                return new int[] {nums[i],i+1};
+            }
+        }
+
+        return new int[] {-1,-1};
+    }
+
+    void cyclicsort(int[] nums){
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i] - 1;
+
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }    
+    
+}
