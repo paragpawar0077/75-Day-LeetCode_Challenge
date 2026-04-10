c7564f646fff35f8b70491787e514ce1e592c98e
Parag Pawar <paragpawar0077@gmail.com>
2026-04-10 19:07:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0) return false;
+        if(n<=0) return false;
         if(n==1) return true;
 
         if(n%2!=0) return false;
