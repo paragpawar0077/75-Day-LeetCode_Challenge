1663367c4f1b628dd9740fd67760a11a0dd21081
Parag Pawar <paragpawar0077@gmail.com>
2026-05-07 11:08:56 +0530

    Time: 3 ms (55.09%), Space: 46.5 MB (57.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,25 +1,32 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean [][] rows = new boolean[9][10];
-        boolean [][] col = new boolean[9][10];
-        boolean [][] boxes = new boolean[9][10];
+            Set<Character>[] rows = new HashSet[9];
+            Set<Character>[] cols = new HashSet[9];
+            Set<Character>[] boxes = new HashSet[9];
 
-        for(int r=0; r<9;r++){
-            for(int c=0;c<9;c++){
-        char val = board[r][c];
-        if(val=='.') continue;
-
-        int num=val -'0';
-        int boxindx = (r/3)*3 + (c/3);
-        if(rows[r][num]) return false;
-        if(col[c][num]) return false;
-        if(boxes[boxindx][num]) return false; 
-
-        rows[r][num]    = true;
-        col[c][num]    = true;
-        boxes[boxindx][num] = true;
+            for(int i=0;i<9;i++){
+                rows[i] = new HashSet<>();
+                cols[i] = new HashSet<>();
+                boxes[i] = new HashSet<>();
             }
-        }
-        return true;
+
+            for(int r =0; r<9;r++){
+                for(int c=0;c<9;c++){
+                    char val = board[r][c];
+
+                    if(val=='.') continue;
+
+                    int boxIndex = (r/3)*3+(c/3);
+
+                    if(rows[r].contains(val)) return false;
+                    if(cols[c].contains(val)) return false;
+                    if(boxes[boxIndex].contains(val)) return false;
+
+                    rows[r].add(val);
+                    cols[c].add(val);
+                    boxes[boxIndex].add(val);
+                }
+            }
+            return true;
     }
 }
