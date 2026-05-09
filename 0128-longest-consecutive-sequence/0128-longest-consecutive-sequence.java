f91ffd45dd88dabf1bdcf783cbd357df233940a4
Parag Pawar <paragpawar0077@gmail.com>
2026-05-09 17:34:32 +0530

    Time: 32 ms (39.85%), Space: 95.7 MB (34.55%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,11 +6,12 @@ class Solution {
         }
         
         int longest=0;
-        
+        int length;
+        int  current;
         for(int num:set){
             if(!set.contains(num-1)){
-               int length=1;
-               int current=num;
+               length=1;
+               current=num;
             
             while(set.contains(current+1)){
                 current++;
