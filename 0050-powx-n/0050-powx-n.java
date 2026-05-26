b8cad9b056b37bb73c8443ffa4c1953f59e7ffbd
Parag Pawar <paragpawar0077@gmail.com>
2026-05-26 21:27:54 +0530

    Time: 0 ms (100%), Space: 48.2 MB (21.33%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N=n;
+
+        if(n<0){
+            x=1/x;
+            N=-N;
+        }
+        return solve(x,N);
+    }
+    public double solve(double x,long n){
+        if(n==0){
+            return 1;
+        }
+        if(n%2==0){
+            return solve(x*x,n/2);
+        }
+        else{
+            return x*solve(x*x,n/2);
+        }
     }
 }
