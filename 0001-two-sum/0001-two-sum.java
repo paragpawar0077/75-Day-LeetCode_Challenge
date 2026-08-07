796bc0225038f76177170c71cc77259272eaae26
Parag Pawar <paragpawar0077@gmail.com>
2026-08-07 23:43:48 +0530

    Time: 2 ms (99.33%), Space: 47.2 MB (40.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+        public int[] twoSum(int[] nums, int target) {
          Map<Integer , Integer> map = new HashMap<>();       
                 for(int i=0; i<nums.length;i++){
                     int complement = target - nums[i];
@@ -10,5 +10,5 @@ class Solution {
                     map.put(nums[i],i);
                 }
             return new int[]{-1,-1};
-    }
+        }
 }
